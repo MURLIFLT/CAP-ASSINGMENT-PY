97ec81978e1fa842743f1d5b802082646618660b
MURLIFLT <91389324+MURLIFLT@users.noreply.github.com>
2022-02-07 11:47:36 +0530

    Add files via upload

--- dict decrising order function.py
+++ dict decrising order function.py
@@ -1,10 +1,10 @@
 def most_frequent(string):
     d = dict()
-    for key in string:
+    for key in  string:
         if key not in d:
             d[key] = 1
         else:
-            d[key] += 1
+            d[key] -= 1
     return d
 
-print (most_frequent('ississippi'))
+print (most_frequent('mississippi'))
